03aeeba854afb13f6817e969bd0857fed014bb28
Kushan Randika Herath <k14922160@gmail.com>
2025-06-17 13:46:18 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,12 @@
-java.mai
+class EmployeeDetails{
+    public static void main(String[] args) {
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("H.M.Kushan Randika Herath","E00123",10000.00,4000);
+
+        System.out.println("FullTime Employee name : " + fullTimeEmployee.getName());
+        System.out.println("FullTime Employee ID : " + fullTimeEmployee.getEmployeeId());
+        System.out.println("FullTime Employee  Base Salary : " + fullTimeEmployee.getBaseSalary());
+        System.out.println("FullTime Employee Monthly Salary : " + fullTimeEmployee.calculateMonthlySalary());
+        System.out.println("FullTime Employee Monthly Salary bonus: " + fullTimeEmployee.calSalary());
+
+    }
+}
