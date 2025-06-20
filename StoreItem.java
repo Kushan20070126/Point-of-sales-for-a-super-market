bee6021a6510463e7ba2d154227b3dc35c167f30
Kushan <k14922160@gmail.com>
2025-06-20 19:08:23 +0530

    I make the Product.java and abstract StoreItem.java and next i include StoreItem class attributes,Constructor,displayInfo abstract method and getters

---
+++ StoreItem.java
@@ -0,0 +1,26 @@
+// host: this class is Super class
+public abstract class StoreItem {
+    // host : define StoreItem attributes
+    protected String id, name;
+    protected double price;
+
+    // host: Make the Constructor for StoreItem
+    StoreItem(String id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+
+    }
+        // host : displayInfo used to be next
+    public abstract void displayInfo();
+
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+    public double getPrice(){
+        return price;
+    }
+}
